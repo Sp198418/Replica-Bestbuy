99adb404aec629e0b36434233674648b60aa0919
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2021-03-26 23:34:45 -0400

    bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const SearchBar = ({keyword,setKeyword}) => {
-  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+  const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem", display:"flexs",};
   return (
+    <div className= "Searchbar">
     <input 
      style={BarStyling}
      key="random1"
@@ -10,6 +11,7 @@ const SearchBar = ({keyword,setKeyword}) => {
      placeholder={"Search Best Buy"}
      onChange={(e) => setKeyword(e.target.value)}
     />
+    </div>
   );
 }
 
