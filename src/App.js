f367e19e822a0ab5a35a0ff63383c3d0766930df
Samuel Pierre Louis <samuelpierrelouis10@gmail.com>
2021-04-06 23:04:00 -0400

    Homepage component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import {Switch} from "react-router-dom"
+// import {Switch} from "react-router-dom";
 import NavBar from './components/Logo/NavBar/NavBar';
+import HomePage from './components/Logo/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
        <NavBar/>
-       <Switch>
-       {/* pages go here */}
-       </Switch>
+       <HomePage/>
+       {/* <Switch>
+      //  {/* pages go here */}
+       {/* </Switch> */}
      
     </div>
   );
--- src/components/Logo/HomePage/Homepage.js
+++ src/components/Logo/HomePage/Homepage.js
@@ -1,21 +1,25 @@
 import "./HomePage.css";
-import React, { Component } from 'react';
+import React, { Jumbotron, Button } from 'react';
 
 
-class HomePage extends Component {
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
-
-
-    render () {
+const HomePage = () => {
         return (
-            <div></div>
+            <div>
+            <Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p>
+  <p>
+    <Button variant="primary">Shop now</Button>
+  </p>
+</Jumbotron>
+</div>
         )
     }
-}
 
 
 
 
+export default HomePage;
